647a88443ca933b5a3fa67913c9cec07a0371e7c
jobalcazar <jbalcazar7@gmail.com>
2022-04-11 23:22:00 -0400

    Add missing columns to purchases to reflected updated table

--- app/models/purchase.py
+++ app/models/purchase.py
@@ -2,26 +2,28 @@ from flask import current_app as app
 
 
 class Purchase:
-    def __init__(self, id, uid, pid, time_purchased):
+    def __init__(self, id, uid, ticker,num_shares, cost, time_purchased):
         self.id = id
         self.uid = uid
-        self.pid = pid
+        self.ticker = ticker
+        self.num_shares=num_shares
+        self.cost=cost
         self.time_purchased = time_purchased
 
     @staticmethod
     def get(uid):
         rows = app.db.execute('''
-        SELECT id, uid, ticker, time_purchased
+        SELECT id, uid, ticker,num_shares,cost, time_purchased
         FROM Purchases
         WHERE uid = :uid
         ''',
-        id=id)
+        uid=uid)
         return [Purchase(*row) for row in rows] if rows else None
 
     @staticmethod
     def get_all_by_uid_since(uid, since):
         rows = app.db.execute('''
-            SELECT id, uid, ticker, time_purchased
+            SELECT  id, uid, ticker,num_shares,cost, time_purchased
             FROM Purchases
             WHERE uid = :uid
             AND time_purchased >= :since
